13613f4841a6bacafe5dee423882c3b6e30fde9d
Marek Dudek <marek.dudek@gmail.com>
2015-02-23 13:38:40 -0800

    Small refactor plus script for running job

--- src/main/java/biz/interretis/hadoop_tutorial/max_temperature/MaxTemperatureMapper.java
+++ src/main/java/biz/interretis/hadoop_tutorial/max_temperature/MaxTemperatureMapper.java
@@ -17,7 +17,7 @@ public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWr
         final String line = value.toString();
 
         final String year = line.substring(15, 19);
-        final int airTemperature = airTemperature(line);
+        final int airTemperature = Integer.parseInt(airTemperature(line));
         final String quality = line.substring(92, 93);
 
         if (airTemperature != MISSING && quality.matches("[01459]")) {
@@ -25,14 +25,12 @@ public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWr
         }
     }
 
-    private int airTemperature(final String line) {
+    private String airTemperature(final String line) {
 
-        String substring;
         if (line.charAt(87) == '+') {
-            substring = line.substring(88, 92);
+            return line.substring(88, 92);
         } else {
-            substring = line.substring(87, 92);
+            return line.substring(87, 92);
         }
-        return Integer.parseInt(substring);
     }
 }
