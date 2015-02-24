1f989cf23a68e8401277e6216353d6337254f2e4
Marek Dudek <marek.dudek@gmail.com>
2015-02-24 13:50:09 -0800

    MR with Combiner in new API

---
+++ src/main/scripts/max-temperature-new-api-job-with-combiner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+hadoop fs -rm /user/marek/max-temperature/output/*
+hadoop fs -rmdir /user/marek/max-temperature/output
+
+hadoop jar target/hadoop-tutorial-1.0-SNAPSHOT.jar \
+    biz.interretis.hadoop_tutorial.max_temperature.new_api.MaxTemperatureNewAPIJobWithCombiner \
+    /user/marek/max-temperature/1901 \
+    /user/marek/max-temperature/output
+
+hadoop fs -cat /user/marek/max-temperature/output/part-r-00000
