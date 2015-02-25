e904cb3207d9904190e2e740382986c7c85d757a
Marek Dudek <marek.dudek@gmail.com>
2015-02-25 13:32:38 -0800

    Wordcount in old API

---
+++ src/main/scripts/word-count/word-count-old-api-job-with-combiner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+hadoop fs -mkdir /user/marek/word-count
+hadoop fs -mkdir /user/marek/word-count/input
+hadoop fs -mkdir /user/marek/word-count/output
+
+
+
+hadoop fs -rm /user/marek/word-count/output/*
+hadoop fs -rmdir /user/marek/word-count/output
+
+hadoop jar target/hadoop-tutorial-1.0-SNAPSHOT.jar \
+    biz.interretis.hadoop_tutorial.word_count.old_api.WordCountOldAPI \
+    /user/marek/word-count/input/gettysburg-address.txt \
+    /user/marek/word-count/output
+
+hadoop fs -cat /user/marek/word-count/output/part-00000
