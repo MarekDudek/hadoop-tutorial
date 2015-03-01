b3d427233194b07f668558671a57b17360057176
Marek Dudek <marek.dudek@gmail.com>
2015-03-01 14:56:01 -0800

    Max temperature tested with whole file

--- src/test/java/biz/interretis/hadoop_tutorial/max_temperature/new_api/MaxTemperatureNewAPITest.java
+++ src/test/java/biz/interretis/hadoop_tutorial/max_temperature/new_api/MaxTemperatureNewAPITest.java
@@ -1,5 +1,6 @@
 package biz.interretis.hadoop_tutorial.max_temperature.new_api;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -7,7 +8,9 @@ import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class MaxTemperatureNewAPITest {
 
@@ -17,6 +20,9 @@ public class MaxTemperatureNewAPITest {
 
     // Test infrastructure
     private MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> driver;
+
+    // Values
+    public static final LongWritable IGNORED_KEY = new LongWritable(0);
 
     @Before
     public void setUp() {
@@ -31,10 +37,26 @@ public class MaxTemperatureNewAPITest {
     public void single_line() throws IOException {
 
         // given
-        driver.withInput(new LongWritable(0), new Text("0029029070999991901010320004+64333+023450FM-12+000599999V0202301N011819999999N0000001N9-00281+99999101741ADDGF108991999999999999999999"));
+        driver.withInput(IGNORED_KEY, new Text("0029029070999991901010320004+64333+023450FM-12+000599999V0202301N011819999999N0000001N9-00281+99999101741ADDGF108991999999999999999999"));
 
         // then
         driver.withOutput(new Text("1901"), new IntWritable(-28));
+
+        // when
+        driver.runTest();
+    }
+
+    @Test
+    public void whole_file() throws IOException {
+
+        // given
+        final List<String> lines = FileUtils.readLines(new File("./src/main/resources/max-temperature/text/1901"));
+        for (final String line : lines) {
+            driver.withInput(IGNORED_KEY, new Text(line));
+        }
+
+        // then
+        driver.withOutput(new Text("1901"), new IntWritable(317));
 
         // when
         driver.runTest();
