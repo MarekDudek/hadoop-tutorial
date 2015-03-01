ee97f2e0841e1292ac78b24cb46cf0ebf0e97637
Marek Dudek <marek.dudek@gmail.com>
2015-03-01 08:29:45 -0800

    bash + Python solution

--- src/main/scripts/max-temperature/max-temperature-python.sh
+++ src/main/scripts/max-temperature/max-temperature-python.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-hadoop fs -rm /user/marek/max-temperature/output/*
-hadoop fs -rmdir /user/marek/max-temperature/output
-
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming-2.5.0-cdh5.3.0.jar \
-    -input   /user/marek/max-temperature/input \
-    -output  /user/marek/max-temperature/output \
-    -mapper  hdfs:///user/marek/max-temperature/python/map.py \
-    -reducer hdfs:///user/marek/max-temperature/python/reduce.py
+cat ./src/main/resources/max-temperature/text/* | \
+    ./src/main/python/max-temperature/map.py | \
+    sort | \
+    ./src/main/python/max-temperature/reduce.py
