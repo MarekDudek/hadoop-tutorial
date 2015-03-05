c1f2802b248a64ffa7b15a714c0630de1ae819e3
Marek Dudek <marek.dudek@gmail.com>
2015-03-05 14:22:00 -0800

    almost got local wordcount to working: performed map failed on reduce

--- src/main/scripts/word-count/word-count-new-api-job-with-combiner-local-fs.sh
+++ src/main/scripts/word-count/word-count-new-api-job-with-combiner-local-fs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-hadoop fs -rm file:///home/cloudera/Devel/IdeaProjects/HadoopTutorial/target/output/*
-hadoop fs -rmdir file:///home/cloudera/Devel/IdeaProjects/HadoopTutorial/target/output
+hadoop fs -rm file:///home/cloudera/Devel/IdeaProjects/hadoop-tutorial/target/output/*
+hadoop fs -rmdir file:///home/cloudera/Devel/IdeaProjects/hadoop-tutorial/target/output
 
 
 hadoop jar ./target/hadoop-tutorial-1.0-SNAPSHOT.jar \
     biz.interretis.hadoop_tutorial.word_count.new_api.WordCountNewAPI \
-    file:///home/cloudera/Devel/IdeaProjects/HadoopTutorial/src/main/resources/word-count/gettysburg-address.txt \
-    file:///home/cloudera/Devel/IdeaProjects/HadoopTutorial/target/output
+    file:///home/cloudera/Devel/IdeaProjects/hadoop-tutorial/src/main/resources/word-count/gettysburg-address.txt \
+    file:///home/cloudera/Devel/IdeaProjects/hadoop-tutorial/target/output
 
-hadoop fs -cat file:///home/cloudera/Devel/IdeaProjects/HadoopTutorial/target/output
+hadoop fs -cat file:///home/cloudera/Devel/IdeaProjects/hadoop-tutorial/target/output
 
