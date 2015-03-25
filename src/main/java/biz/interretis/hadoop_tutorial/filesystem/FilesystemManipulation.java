d4c93c2d26b9b13f522e85cd776c52f5a2977537
Marek Dudek <marek.dudek@gmail.com>
2015-03-25 13:47:04 -0700

    HDFS accessed via API

---
+++ src/main/java/biz/interretis/hadoop_tutorial/filesystem/FilesystemManipulation.java
@@ -0,0 +1,66 @@
+package biz.interretis.hadoop_tutorial.filesystem;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+public class FilesystemManipulation {
+
+    public static class FileSystemMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+
+            super.setup(context);
+
+            URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+            printLines();
+        }
+    }
+
+    public static void main(final String... args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+        printLines();
+
+        final Configuration config = new Configuration();
+
+        final Job job = Job.getInstance(config, "Filesystem manipulation");
+
+        final String input = args[0];
+        final String output = args[1];
+
+        FileInputFormat.addInputPath(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+
+        job.setJarByClass(FilesystemManipulation.class);
+
+        job.setMapperClass(FileSystemMapper.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    private static void printLines() throws IOException {
+
+        final URL fileUrl = new URL("hdfs://localhost/user/marek/filesystem/input/counties.csv");
+
+        final InputStream stream = fileUrl.openStream();
+        final List<String> lines = IOUtils.readLines(stream);
+        for(final String line: lines) {
+            System.out.println(line);
+        }
+        IOUtils.closeQuietly(stream);
+    }
+}
---
+++ src/main/scripts/filesystem/filesystem-manipulation.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+hadoop jar target/hadoop-tutorial-1.0-SNAPSHOT.jar \
+    biz.interretis.hadoop_tutorial.filesystem.FilesystemManipulation \
+    /user/marek/filesystem/input \
+    /user/marek/filesystem/output
